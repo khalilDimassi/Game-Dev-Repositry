b9ce82f97bb28f264b434333186f460bd8461cea
khalil dimassi <dimassi0khalil@gmail.com>
2021-03-01 11:30:21 +0100

    redesigning landing page

--- LaravelWebsite/webApp/resources/views/landing.blade.php
+++ LaravelWebsite/webApp/resources/views/landing.blade.php
@@ -1,60 +1,99 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Wake Me Up</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@section('content')
-<div class="container">
-    
-    <section class="section-head">
-        <div class="head-background"></div>
-        <div class="over"></div>
-    </section>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
 
-    <section class="section-1">
-        <div class="grid">
-            <div class="row">
-
+      <div class="container-fluid">
+        <section class="container nav-section my-5 py-3" style="height: 100vh">
+          <div class="row px-5 py-2 align-items-center justify-content-around" style="width: 100%; height: 20%">
+            
+            <div class="col-3 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
             </div>
-        </div>
-    </section>
-
-    <section class="section-2">
-        <div class="grid">
-            <div class="row">
-
+            
+            <div class="col-3 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
             </div>
-        </div>
-    </section>
-
-    <section class="section-3">
-        <div class="grid">
-            <div class="row">
-
+            
+            <div class="col-3 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
             </div>
-        </div>
-    </section>
 
-    <section class="section-4">
-        <div class="grid">
-            <div class="row">
-
+          </div>
+          <div class="row px-5 py-2 align-items-center justify-content-around" style="width: 100%; height: 20%">
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
             </div>
-        </div>
-    </section>
-
-    <section class="section-5">
-        <div class="grid">
-            <div class="row">
-
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
             </div>
-        </div>
-    </section>
-
-    <section class="section-6">
-        <div class="grid">
-            <div class="row">
-
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
             </div>
-        </div>
-    </section>
-    
-</div>
-@endsection
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+          </div>
+          
+          <div class="row px-5 py-2 align-items-center justify-content-center" style="width: 100%; height: 20%">
+            <a href="#">
+              <button class="btn btn-primary">click me!</button>
+            </a>
+          </div>
+          
+          <div class="row px-5 py-2 align-items-center justify-content-around" style="width: 100%; height: 20%">
+            
+            <div class="col-3 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+            
+            <div class="col-3 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+            
+            <div class="col-3 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+
+          </div>
+          <div class="row px-5 py-2 align-items-center justify-content-around" style="width: 100%; height: 20%">
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+          </div>
+        </section>
+        
+        <section class="section-1"></section>
+        <section class="section-2"></section>
+        <section class="section-3"></section>
+        <section class="section-4"></section>
+        <section class="section-5"></section>
+      </div>
+
+
+    <!-- Optional JavaScript -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
