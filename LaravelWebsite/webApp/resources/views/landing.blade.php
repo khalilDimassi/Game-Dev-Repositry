30b7cca21c19e409b4cd8b6a371b11faf7ea3b42
khalil dimassi <dimassi0khalil@gmail.com>
2021-03-01 16:05:04 +0100

    ANIMATED IT

--- LaravelWebsite/webApp/resources/views/landing.blade.php
+++ LaravelWebsite/webApp/resources/views/landing.blade.php
@@ -8,16 +8,28 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <!-- my CSS -->
+    <link rel="stylesheet" href="{{ asset('css/landing.css') }}" type="text/css">
   </head>
   <body>
       
 
       <div class="container-fluid">
         <section class="container nav-section my-5 py-3" style="height: 100vh">
+
           <div class="row px-5 py-2 align-items-center justify-content-around" style="width: 100%; height: 20%">
             
             <div class="col-3 justify-content-center align-items-center d-flex h-100">
-              <a href="#"><button>click me!</button></a>
+              <a href="#" class="d-flex justify-content-center align-items-center">
+                <span class="deco">
+                  <img class="i1" src="svg\anim\Deco-1.svg" alt="deco">
+                </span>
+                
+                <span class="deco">
+                  <img class="i2" src="svg\anim\Deco-1.svg" alt="deco">
+                </span>
+              </a>
             </div>
             
             <div class="col-3 justify-content-center align-items-center d-flex h-100">
@@ -25,7 +37,11 @@
             </div>
             
             <div class="col-3 justify-content-center align-items-center d-flex h-100">
-              <a href="#"><button>click me!</button></a>
+              <a href="#">
+                <span class="panels">  
+                  <img src="svg\anim\signin.svg" alt="signin">
+                </span>
+              </a>
             </div>
 
           </div>
@@ -44,12 +60,40 @@
             </div>
           </div>
           
-          <div class="row px-5 py-2 align-items-center justify-content-center" style="width: 100%; height: 20%">
-            <a href="#">
-              <button class="btn btn-primary">click me!</button>
-            </a>
+          <div class="row px-5 py-2 align-items-center justify-content-around" style="width: 100%; height: 20%">
+            
+            <div class="col-2 h-100"></div>
+            <div class="col-2 h-100"></div>
+            <div class="col-2 h-100 d-flex align-items-center justify-content-center">
+              <a class="a-start d-flex align-items-center justify-content-center" href="#">
+                <span id="L6"> <img src="svg\anim\layer-6.svg" alt="lay6"> </span>
+                <span id="L5"> <img src="svg\anim\layer-5.svg" alt="lay6"> </span>
+                <span id="L4"> <img src="svg\anim\layer-4.svg" alt="lay6"> </span>
+                <span id="L3"> <img src="svg\anim\layer-3.svg" alt="lay6"> </span>
+                <span id="L2"> <img src="svg\anim\layer-2.svg" alt="lay6"> </span>
+                <span id="L1"> <img src="svg\anim\layer-1.svg" alt="lay6"> </span>
+                <span id="sb"> <img src="svg\anim\center.svg" alt="cb"> </span>
+                <span id="start-btn"> <img src="svg\anim\symbol.svg" alt="sb"> </span>
+              </a>
+            </div>
+            <div class="col-2 h-100"></div>
+            <div class="col-2 h-100"></div>
           </div>
           
+          <div class="row px-5 py-2 align-items-center justify-content-around" style="width: 100%; height: 20%">
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+            <div class="col-2 justify-content-center align-items-center d-flex h-100">
+              <a href="#"><button>click me!</button></a>
+            </div>
+          </div>
           <div class="row px-5 py-2 align-items-center justify-content-around" style="width: 100%; height: 20%">
             
             <div class="col-3 justify-content-center align-items-center d-flex h-100">
@@ -64,20 +108,6 @@
               <a href="#"><button>click me!</button></a>
             </div>
 
-          </div>
-          <div class="row px-5 py-2 align-items-center justify-content-around" style="width: 100%; height: 20%">
-            <div class="col-2 justify-content-center align-items-center d-flex h-100">
-              <a href="#"><button>click me!</button></a>
-            </div>
-            <div class="col-2 justify-content-center align-items-center d-flex h-100">
-              <a href="#"><button>click me!</button></a>
-            </div>
-            <div class="col-2 justify-content-center align-items-center d-flex h-100">
-              <a href="#"><button>click me!</button></a>
-            </div>
-            <div class="col-2 justify-content-center align-items-center d-flex h-100">
-              <a href="#"><button>click me!</button></a>
-            </div>
           </div>
         </section>
         
