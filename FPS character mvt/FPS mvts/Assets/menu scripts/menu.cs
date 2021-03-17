50b2a240bf9d789bf6c50cf9806a8bdf79d9b24c
khalil dimassi <dimassi0khalil@gmail.com>
2021-03-17 17:46:00 +0100

    first networking test, Works
    creating and joining sessions, spawning players, initial test

--- FPS character mvt/FPS mvts/Assets/menu scripts/menu.cs
+++ FPS character mvt/FPS mvts/Assets/menu scripts/menu.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Bolt;
+using Bolt.Matchmaking;
+using UdpKit;
 
-public class menu : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class menu : GlobalEventListener
+{   
+
+    //called from host game button
+    public void startServer()
     {
-        
+        BoltLauncher.StartServer();
     }
 
-    // Update is called once per frame
-    void Update()
+    //start session
+    public override void BoltStartDone()
     {
-        
+        BoltMatchmaking.CreateSession(sessionID: "testSession", sceneToLoad: "testGame");
+    }
+
+    //called from join game button
+    public void startClient()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    //client loop through session and join the first available one
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltMatchmaking.JoinSession(photonSession);
+            }
+        }
     }
 }
---
+++ FPS character mvt/FPS mvts/Assets/menu scripts/networkCallBacks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Bolt;
+
+public class networkCallBacks : GlobalEventListener
+{
+    public GameObject spherePrefab;
+
+    public override void SceneLoadLocalDone(string scene, IProtocolToken ipt)
+    {
+        var SpawnPos = new Vector3(Random.Range(-8, 8), 2, Random.Range(-8, 8));
+
+        BoltNetwork.Instantiate(spherePrefab, SpawnPos, Quaternion.identity);
+
+    }
+}
