4d149394cfbb26d2a958db70a7944cad5ee49136
khalil dimassi <dimassi0khalil@gmail.com>
2021-03-18 16:09:05 +0100

    health and player deletion test

---
+++ FPS character mvt/FPS mvts/Assets/humanoid rigs/player preFab/scripts/playerHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class playerHealth : Bolt.EntityBehaviour<IcustomSphereS>
+{
+    public int localHealth = 3;
+
+    //void start():
+    public override void Attached()
+    {
+        state.health = localHealth;
+        state.AddCallback("health", healthCallback);
+    }
+
+    //syncronise local health with the network
+    private void healthCallback()
+    {
+        localHealth = state.health;
+
+        if (localHealth <= 0)
+        {
+            BoltNetwork.Destroy(gameObject);
+        }
+    }
+
+    //change health locally
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            state.health -= 1;
+        }
+    }
+}
