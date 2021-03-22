4cce8f5cf1438cc8f84603d32c099fc4edf44030
khalil dimassi <dimassi0khalil@gmail.com>
2021-03-22 17:11:32 +0100

    started networking on unreal engine

---
+++ FPS character mvt/FPS mvts/Assets/envirement/scripts/serverEventListener.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class serverEventListener : Bolt.GlobalEventListener
+{
+    public override void OnEvent(playerJoinedEvent joinEvent)
+    {
+        Debug.Log(joinEvent.joinLog);
+    }
+}
---
+++ FPS character mvt/FPS mvts/Assets/humanoid rigs/player preFab/scripts/playerJoinLog.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class playerJoinLog : Bolt.EntityBehaviour<IcustomSphereS>
+{
+    public override void Attached()
+    {
+        var joinEvent = playerJoinedEvent.Create();
+        joinEvent.joinLog = "i joined";
+        joinEvent.Send();
+    }
+}
