5f16ba9f64a5b719792e2bfe898614d0a0f56491
tasnimkassebi <79140197+tasnimkassebi@users.noreply.github.com>
2021-06-12 10:22:48 -0700

    Create DestroyItem.cs

---
+++ MOBA_networking_setup/MOBA/Assets/Flooded_Grounds/Scripts/DestroyItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void ontriggerEnter(collider other)
+    { if (other.tag == "player")
+        {
+            Destroy(objToDestroy);
+        }
+        Destroy
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
